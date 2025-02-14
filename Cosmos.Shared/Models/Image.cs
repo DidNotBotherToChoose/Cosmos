8eb730a8cbd0a1deec475c61fbd453ee661e2096
CamillyAM <camillyalmeidademoura@gmail.com>
2025-02-14 19:28:42 +0000

    implementação de entidades na pasta models

---
+++ Cosmos.Shared/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Shared.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public string URL { get; set; }
+    }
+}
